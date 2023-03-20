24438e4304794d2de0d0573180b3554ac9b2f100
sseergO <s.ivanov@fbs.com>
2023-03-20 04:07:15 +0300

    bonus

--- js/script.js
+++ js/script.js
@@ -215,7 +215,8 @@ const renderTime = (wrapper, data) => {
 
 const initReserve = () => {
     const reserveForm = document.querySelector('.reserve__form');
-    const {fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn} = reserveForm;
+    const {fieldservice, fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn} = reserveForm;
+    
     addDisabled([fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn]);
 
     reserveForm.addEventListener('change', async (event) => {
@@ -270,7 +271,44 @@ const initReserve = () => {
         if (target.name === 'time') {
             removeDisabled([btn]);
         }
-    })
+    });
+
+    reserveForm.addEventListener('submit', async(e) => {
+        e.preventDefault();
+
+        const formData = new FormData(reserveForm);
+        const json = JSON.stringify(Object.fromEntries(formData));
+    
+        const response = await fetch(`${API_URL}api/order`, {
+            method: 'post',
+            body: json,
+        });
+
+        const data = await response.json();
+        
+        addDisabled([
+            fieldservice, 
+            fieldspec, 
+            fielddata, 
+            fieldmonth, 
+            fieldday, 
+            fieldtime, 
+            btn,
+        ]);
+
+        const p = document.createElement('p');
+        p.textContent = `
+            Спасибо за бронь #${data.id}!
+            Ждем вас ${new Intl.DateTimeFormat('ru-RU', {
+                month: 'long',
+                day: 'numeric',
+
+            }).format(new Date(`${data.month}/${data.day}`))},
+            время ${data.time}
+        `;
+
+        reserveForm.append(p);
+    });
 };
 
 const init = () => {
